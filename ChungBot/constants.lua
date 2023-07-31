f2164308f40a0f4e280f42f2e5e8bd2817d1768c
Christian <chrille_0313@hotmail.com>
2023-07-31 23:15:43 +0200

    Create constants.lua

---
+++ ChungBot/constants.lua
@@ -0,0 +1,5 @@
+DIRECTIONS = {"north", "east", "south", "west"}
+
+NORTH, EAST, SOUTH, WEST, UP, DOWN = 1, 2, 3, 4, 5, 6
+
+DELTA = {vector.new(0, 0, -1), vector.new(1, 0, 0), vector.new(0, 0, 1), vector.new(-1, 0, 0), vector.new(0, 1, 0), vector.new(0, -1, 0)}
